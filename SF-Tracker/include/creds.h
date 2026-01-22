c0f27d861fb9c838df4d93411fa4216c67657c78
JTB <Donutnz@users.noreply.github.com>
2026-01-22 13:04:59 +1300

    Removed details from creds

--- SF-Tracker/include/creds.h
+++ SF-Tracker/include/creds.h
@@ -2,11 +2,11 @@
  * Network creds
  */
 
-const char* hiveMQ_Addr = "571950dde3e941329c5b3feec9b03fbc.s1.eu.hivemq.cloud";
+const char* hiveMQ_Addr = "";
 #define HIVEMQ_PORT 8883
 
-const char* hiveMQ_UName = "SF_Tracker";
-const char* hiveMQ_Pass = "SyncFlower1";
+const char* hiveMQ_UName = "";
+const char* hiveMQ_Pass = "";
 
-const char* wifi_SSID = "Donut.net";
-const char* wifi_Pass = "ejtd1391";
+const char* wifi_SSID = "";
+const char* wifi_Pass = "";
